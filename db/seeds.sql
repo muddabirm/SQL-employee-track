261f0b17e7d133327ccd5ffdfdc43aadf726ebc3
muddabirm <33209109+muddabirm@users.noreply.github.com>
2023-11-13 05:30:47 -0600

    fixed error with schema

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO department (name,id)
+INSERT INTO department (id,name)
 VALUES  (1,"Finance"),
         (2,"Sales"),
         (3,"Accounting"),
@@ -6,13 +6,13 @@ VALUES  (1,"Finance"),
         (5,"Management");
 
 INSERT INTO role (id,title,salary,department_id)
-VALUES  (1,"Finance Lead",25,22)
-        (2,"Sales Lead",30,23)
-        (3,"Marketing Lead",40,24)
-        (4,"Management Lead",50,25);
+VALUES  (1,"Management",25,22),
+        (2,"Teller",30,23),
+        (3,"Lead",40,24),
+        (4,"TeamLead",50,25);
 
 INSERT INTO employee (id,first_name,last_name,role_id,manager_id)
-VALUES  (1,"Carl","Marks",10,12)
-        (2,"Mason","Shocks",12,23)
-        (3,"Jane","Doe",14,22)
+VALUES  (1,"Carl","Marks",10,12),
+        (2,"Mason","Shocks",12,23),
+        (3,"Jane","Doe",14,22),
         (4,"bob","bobby",7,8);
