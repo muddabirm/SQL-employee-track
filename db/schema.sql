2eebfc8228595ed00134a2ed4c4c5d3d59e41f27
muddabirm <33209109+muddabirm@users.noreply.github.com>
2023-11-13 04:42:58 -0600

    initial init

---
+++ db/schema.sql
@@ -0,0 +1,21 @@
+DROP DATABASE IF EXISTS manager_db;
+CREATE DATABASE manager_db;
+
+CREATE TABLE department(
+    id INT,
+    name VARCHAR(30)
+);
+CREATE TABLE role(
+    id INT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT
+
+);
+CREATE TABLE employee(
+    id INT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT
+);
