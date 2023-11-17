86c21520e57b016e29c6a9f43bef9f7797648108
muddabirm <33209109+muddabirm@users.noreply.github.com>
2023-11-17 10:35:36 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import mysql from 'mysql2'
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'Stewbeefcake1232!@#',
+  password: '',
   database: 'manager_db'
 },
   console.log('connected')
